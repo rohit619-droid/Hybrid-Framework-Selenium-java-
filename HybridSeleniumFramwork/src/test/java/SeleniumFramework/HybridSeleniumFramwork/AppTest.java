5c2afdbe14ef1df9b3df2a5086dc42fefd0f043c
rohit619-droid <rohitkuruvilla2@gmail.com>
2020-04-27 10:38:02 +0530

    Initaiated New Hybrid Framework - Selenium with java.

---
+++ HybridSeleniumFramwork/src/test/java/SeleniumFramework/HybridSeleniumFramwork/AppTest.java
@@ -0,0 +1,20 @@
+package SeleniumFramework.HybridSeleniumFramwork;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
